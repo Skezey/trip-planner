c6aa03aa767a4191b7e5a0e10fdbd86f46798514
Christian Cuartas <christian.cuartas@gmail.com>
2019-09-07 14:12:03 -0600

    completed view location addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-=======
->>>>>>> 9474c9492f5d2af0bf89d9fb9af094df58d9d548
 name = ['Trip 1', 'Trip 2', 'Trip 3']
 date = Faker::Date.between(from: 1.year.ago, to: 1.year.from_now)
 
@@ -41,9 +30,5 @@ date = Faker::Date.between(from: 1.year.ago, to: 1.year.from_now)
   end
 end
 
-<<<<<<< HEAD
-puts 'Date seeded'
-=======
 puts 'Date seeded'
 
->>>>>>> 9474c9492f5d2af0bf89d9fb9af094df58d9d548
