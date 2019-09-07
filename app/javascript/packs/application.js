2767b71d5bab0a1553e0129da1918bc400103ba4
Scott Young <youngscottj@gmail.com>
2019-09-07 09:53:29 -0600

    add gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+//=require jquery3
+//=require popper
+//=require bootstrap-sprockets
