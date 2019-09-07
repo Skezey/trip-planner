8b57635667d5e20c7f4ac754bd48b2c5703ee589
Scott Young <youngscottj@gmail.com>
2019-09-07 12:40:46 -0600

    add styling to index tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
 
   root 'trips#index'
-<<<<<<< HEAD
-  
+
   resources :trips
 
-=======
-  resources :trips
-  
->>>>>>> 9474c9492f5d2af0bf89d9fb9af094df58d9d548
   resources :locations do
     resources :addresses
   end
